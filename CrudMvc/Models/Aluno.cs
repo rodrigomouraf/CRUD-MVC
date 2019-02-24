19aa93ac5933df907b23179b5695258895c40946
Rodrigo Moura <rodrigomoura_rm@hotmail.com>
2019-02-23 23:10:01 -0300

    Versão 1.0

---
+++ CrudMvc/Models/Aluno.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CrudMvc.Models
+{
+    [Table("Alunos")]
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        [Display(Name = "Endereço")]
+        public string Endereco { get; set; }
+
+        [Required]
+        public string Telefone { get; set; }
+
+        [Required]
+        [Display(Name = "E-mail")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Data de Nascimento")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode =true)]
+        public DateTime Nascimento { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
